6f6ea6eae7ef9cc69a354c8b29bf12aaf6fa1994
Marek Kobyliński <marek.kobylinski@johnpitcher.pl>
2013-10-16 07:29:24 +0200

    gruntfile fix

--- gruntfile.js
+++ gruntfile.js
@@ -6,17 +6,6 @@ module.exports = function (grunt) {
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
     grunt.loadTasks('tasks/');
 
-    var config = {
-        aliases: {
-            'bootstrap' : 'twitter/bootstrap'
-        },
-        subPaths: {
-            //'twitter/bootstrap' : 'dist'
-        },
-        baseDir: 'vendor'
-    }
-
-
     grunt.initConfig({
         publicVendor: {
             options:{
